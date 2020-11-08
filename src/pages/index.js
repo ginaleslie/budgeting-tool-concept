00610c242ce5baaef9f3572a06213d025f9d1802
zolovelani <vumz168@ada.ac.za>
2020-11-08 23:06:48 +0200

    Charts
    Added a few charts from chart.js to see which would b most suitable for app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import './Chart.css'
 import LocalStorage from '../components/LocalStorage'
+import LineChart from '../components/LineChart'
+import BarChart from '../components/BarChart'
+import DoughnutChart from '../components/DoughnutChart'
+
 
 export default function Home() {
   return (
     <div>
       Hello developers!
       <LocalStorage />
+      <div className='chart'>
+       <LineChart /> 
+       <BarChart /> 
+       <DoughnutChart />
+      </div>
     </div>
   )
 }
