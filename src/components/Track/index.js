d10b09fc0d0de8e334317a7d0b6d4a4cb3fd138d
Gina <ginal@indiefin.com>
2020-11-16 21:03:40 +0200

    updated arrow styling

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react"
 import "./styles.css"
+import "typeface-roboto"
 import Copy from "../Copy"
 import Heading from "../Heading"
 import BarChartCategories from "../BarChartCategories"
@@ -143,7 +144,7 @@ const Track = () => {
               setDisplayCategoryTransactions(false)
             }}
           >
-            <i className="arrow-back left"></i>
+            <i className="arrow-back arrow-left"></i>
             Back
           </button>
           <div className="track-flex-money">
@@ -192,13 +193,6 @@ const Track = () => {
                       transaction.category = "household"
                       transaction.color = "#76C2B6"
                     }}
-                    // category={transaction.category}
-                    // changeCategory={() =>
-                    //   //   {
-                    //   //   transaction.category = "food"
-                    //   // }
-                    //   setDisplayChangeCategory(true)
-                    // }
                   />
                 ))
             : null}
