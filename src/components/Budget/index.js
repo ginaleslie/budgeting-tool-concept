7e02a3b527103dc3c1e1a5033a45bb0c58b06463
FaithRuvimbo <ruvimboc@sanlamindie.co.za>
2020-11-22 18:24:33 +0200

    Adding information onto budget
    Added needhelp to budget

--- src/components/Budget/index.js
+++ src/components/Budget/index.js
@@ -6,30 +6,30 @@ import Face from  "./Face"
 import Warning1 from  "./Warning1"
 import Warning2 from  "./Warning2"
 import YourBudget from  "./YourBudget"
+import Needhelp2 from  "./Needhelp2"
+import ChartBox from  "./ChartBox"
 
 
 
 const Budget = () => (
-  
 
+
+<div className='budget-max-width'>
   <div className='box'>
-
-    
-
-  <div className="grid-container2">
-              <div className="grid-item2">
+    <div className="grid-container">
+              <div className="grid-item">
               <p className='catagory color-grey300'>
-              <div className='info-dot2 green'></div>
               <br></br>
               <YourBudget/>
               </p>
             
           </div>
-          <div className="grid-item2">
+          <div className="grid-item">
               <p className='catagory error-400'>
               <h1>Girlfriend's birthday</h1>
               <br></br>
-              <div className='amount-1'><Warning2/> 20 days until Girlfriend’s birthday</div>
+              <div className='amount-1'>
+                <Warning2/> 20 days until Girlfriend’s birthday</div>
               
               </p>
               
@@ -38,9 +38,8 @@ const Budget = () => (
               </p>
           </div>
          
-          <div className="grid-item2">
+          <div className="grid-item">
               <p className='catagory color-grey300'>
-              <div className='info-dot2 green'></div>
               <h1>Playstation 5</h1>
               <br></br>
               <Warning1/> 100 days until playstation 5
@@ -55,25 +54,46 @@ const Budget = () => (
             <h2>
               Summary
             </h2>
+            </div>
 
-          </div>
+          <div className='budget-container-flex'>
+      
 
-          <div>
-          <div class="grid-item2"><h1>Consistency is key, Thabiso</h1>
-          <div class="col-12 col-sm-10">
-          <Face/>You had quite a good start, 
-          however, your biggest drawback is spending on fuel.
-           To reach your goals, you'll need to cut back on fuel spend. Here's how, <a href="default.asp" target="_blank"> Read for more
-            tips.</a> </div>
+
+        <div> 
+          <div class="grid-item">
+          <h1><Face/>Consistency is key, Thabiso  <br></br> <br></br>You had quite a good start, however, your biggest drawback is spending on fuel. 
+            To reach your goals, you'll need to cut back on fuel spend. Here's how,  Read for more tips</h1>
+        </div>
+
+
+        <br></br>
+ <div class="need">
+ <Needhelp2/>
+          
+ </div>
+        
+
+ <br></br>
+ <br></br>
+
+ <div class="chart">
+ <ChartBox/>
+          
+ </div>
 
           </div>
           
-          </div>
-          <p className= ' catagory wealth-grey-300'>Need help with financial planning?<a href="default.asp" target="_blank"> Request a call </a></p>
 
       </div>
       
 </div>
+
+
+</div>
+  
+
+</div>  
 
    
 )
